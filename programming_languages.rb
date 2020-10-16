ecbe0a8ef4ee9458a33d8547955d5527b692d9cb
Michael Kim <m.kim4247@gmail.com>
2020-10-16 20:19:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,35 +5,8 @@ def reformat_languages(languages)
     style.each do |lang, value|
       new_hash[lang] = value
       new_hash[lang][:style] = [key]
-    end 
-  end 
+    end
+  end
   new_hash[:javascript][:style] << :oo
-  new_hash 
+  new_hash
 end
-
-
-# languages = {
-#   :oo => {
-#     :ruby => {:type => "interpreted"},
-#     :javascript => {:type => "interpreted"},
-#     :python => {:type => "interpreted"},
-#     :java => {:type => "compiled"}
-#   },
-#   :functional => {
-#     :clojure => {:type => "compiled"},
-#     :erlang => {:type => "compiled"},
-#     :scala => {:type => "compiled"},
-#     :javascript => {:type => "interpreted"}
-#   }
-# }
-
-# {
-#   :ruby => {:type => "interpreted", :style => [:oo]},
-#   :javascript => {:type => "interpreted", :style => [:oo, :functional]},
-#   :python => {:type => "interpreted", :style => [:oo]},
-#   :java => {:type => "compiled", :style => [:oo]},
-#   :clojure => {:type => "compiled", :style => [:functional]},
-#   :erlang => {:type => "compiled", :style => [:functional]},
-#   :scala => {:type => "compiled",:style => [:functional]}
-# }
-
